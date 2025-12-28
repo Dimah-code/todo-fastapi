dff85ac07c0600165510ba4a5d5348c6dbc7ac5a
dimah <dimah.code@gmail.com>
2025-12-28 09:31:36 +0330

    feat: add get_todos function to return all todos

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 
+from . import models
+from .schemas import TodoResponse, TodoCreate
+
+
+from typing import List
+
 app = FastAPI()
 
 
 @app.get("/")
 def root():
     return {"message": "Todo app is running."}
+
+
+@app.get("/todos", response_model=List[TodoResponse], tags=["Todos"])
+def get_todos():
+    return models.todos_list()
