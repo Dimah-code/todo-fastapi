933aac32bd849efe416e2bf180e4609c4e09df3f
dimah <dimah.code@gmail.com>
2025-12-28 09:25:00 +0330

    feat: add create_todo function to models.py

--- app/models.py
+++ app/models.py
@@ -3,3 +3,17 @@ from typing import List
 from .schemas import TodoResponse
 
 TODOS: List[TodoResponse] = []
+_counter = 0
+
+
+def create_todo(data: TodoCreate) -> TodoResponse:
+    global _counter
+    _counter += 1
+
+    todo = TodoResponse(id=_counter, **data.dict())
+    TODOS.append(todo)
+    return todo
+
+
+def todos_list():
+    return TODOS
